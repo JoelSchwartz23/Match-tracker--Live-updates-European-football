69a19f54b573bccdcac572f13a2a4bf27c3e4c7d
Joel Schwartz <nederlands18@gmail.com>
2019-03-24 21:04:40 -0600

    push

--- src/store.js
+++ src/store.js
@@ -1,16 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import router from './router';
+import { match } from 'minimatch';
 
+let footballapi = axios.create({
+  baseURL: "https://api.football-data.org/v2/competitions/CL/matches"
+})
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    match: {}
 
   },
   mutations: {
+    setMatch(state, match) {
+      state.match = match
+    }
 
   },
   actions: {
-
+    getMatch({ commit }, matchId) {
+      footballapi.get('/' + matchId)
+        .then(res => {
+          commit('setMatch', res.data)
+        })
+        .catch(err => console.error(err.message))
+    }
   }
 })
